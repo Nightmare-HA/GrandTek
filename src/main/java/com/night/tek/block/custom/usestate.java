a1c97c70ae8753c9462d14649e3b94d261f25313
Nightmare <burdickavery@gmail.com>
2023-06-11 10:33:53 -0500

    e

--- src/main/java/com/night/tek/block/custom/usestate.java
+++ src/main/java/com/night/tek/block/custom/usestate.java
@@ -1,7 +1,12 @@
-package com.night.tek.block.custom
+package com.night.tek.block.custom;
 
 import net.minecraft.block.Block;
 
 public abstract class usestate extends Block {
+    public static float USE_PROPERTY = BlockStateProperties.;
+
+    public usestate(Properties properties) {
+        super(properties);
+    }
 
 }
